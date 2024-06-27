676ef975d664ac349d369896edee247e7e7d1b4c
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2024-06-28 00:05:19 +0600

    transition smoothen

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -16,44 +16,54 @@ const SideBar = ({ closeSidebar, setCloseSidebar }) => {
 
   return (
     <div
-      className={` fixed md:static min-h-screen h-full bg-white flex justify-center py-5 transition-all duration-300 ease-in-out ${
-        !closeSidebar ? "border px-4 " : " border p-2"
-      } z-10  `}
+      className={` fixed md:static min-h-screen h-full bg-white flex justify-start py-5 transition-all duration-300 ease-in-out ${
+        !closeSidebar ? " w-64 px-4 " : "  w-16 p-2"
+      } z-10 border `}
     >
-      <ul className={`"space-y-5"`}>
+      <ul className={`space-y-5 w-full`}>
         {routes?.map((item) => {
           // console.log(item?.path);
           return (
             <li
               onClick={() => navigate(item?.path)}
               key={item?.path}
-              className={`${
-                currentUrl?.pathname === item?.path ? "bg-blue-900 text-white" : ""
-              } flex items-center justify-center h-12  rounded-md cursor-pointer ${!closeSidebar && "gap-2"} px-3 `}
+              className={`${currentUrl?.pathname === item?.path ? "bg-blue-900 text-white" : ""} ${
+                closeSidebar ? "" : "justify-center"
+              } flex items-center  h-12  rounded-md cursor-pointer ${!closeSidebar && "gap-2"} px-3 `}
             >
-              <span
-                title={closeSidebar ? item.name : ""}
-                onClick={() => navigate(item?.path)}
-                className="text-2xl flex items-center justify-center"
-              >
+              <span title={closeSidebar ? item.name : ""} onClick={() => navigate(item?.path)} className="text-2xl ">
                 {item?.icons}
               </span>
 
-              <span className={`${closeSidebar ? "hidden" : ""} block w-full`}> {item?.name} </span>
+              <span
+                className={`${
+                  closeSidebar ? "opacity-0" : "opacity-100"
+                } transition-opacity duration-500 ease-in-out block w-full`}
+              >
+                {" "}
+                {item?.name}{" "}
+              </span>
             </li>
           );
         })}
         <li
           title={closeSidebar ? "Open Menu" : ""}
-          className="flex items-center justify-center h-12  rounded-md cursor-pointer md:px-3 "
+          className={` inline-block whitespace-nowrap h-12  rounded-md cursor-pointer md:px-3 `}
         >
           <button
-            className=" flex items-center justify-center gap-[0.3rem] sm:gap-0"
+            className="flex items-center justify-center gap-[0.3rem] sm:gap-0"
             onClick={() => setCloseSidebar((prev) => !prev)}
           >
-            <FaRegArrowAltCircleRight className={`text-3xl  bg-white ${closeSidebar ? "hidden" : ""}`} />
-            <FaRegArrowAltCircleLeft className={`text-2xl  bg-white ${closeSidebar ? "" : "hidden"}`} />
-            <span className={`${closeSidebar ? "hidden" : ""} block w-full  `}> Close Menu </span>
+            <FaRegArrowAltCircleRight className={`text-3xl  bg-white ${closeSidebar ? "hidden" : "block"}`} />
+            <FaRegArrowAltCircleLeft className={`text-3xl  bg-white ${closeSidebar ? "block" : "hidden"}`} />
+            <span
+              className={`${
+                closeSidebar ? "opacity-0" : "opacity-100"
+              } block w-full duration-500 ease-in-out transition-opacity `}
+            >
+              {" "}
+              Close Menu{" "}
+            </span>
           </button>
         </li>
       </ul>
